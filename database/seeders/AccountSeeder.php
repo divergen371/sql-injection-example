8dcd592e2c0756928ad6c3bfed82580f20edd1e1
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-02 15:51:55 +0900

    :seedling: マイグレーションとシードデータの送り込み

---
+++ database/seeders/AccountSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     * 
+     * @return  void
+     */
+    public function run(): void
+    {
+        DB::table("accounts")->insert([
+            "login_id" => "login-user",
+            "password" => "password",
+        ]);
+    }
+}
